ab6fa73897a8b273f44b8c6471f8ad6d5046a349
Zulfiqar Ali <8zali9@gmail.com>
2023-11-14 17:20:51 +0500

    Task Routes Protected & tested.

--- server/routes/userTaskRoutes.js
+++ server/routes/userTaskRoutes.js
@@ -12,25 +12,26 @@ const {
   updateTask,
   deleteTask,
 } = require("../controllers/userTaskControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
 // @route   GET /api/usertasks/:uid
 // @desc    Get all tasks of a user
-router.get("/:uid", getAllTasks);
+router.get("/:uid", protect, getAllTasks);
 
 // @route   GET /api/usertasks/:uid/:tid
 // @desc    Get a single task of a user
-router.get("/:uid/:tid", getTask);
+router.get("/:uid/:tid", protect, getTask);
 
 // @route   POST /api/usertasks/:uid
 // @desc    Create a task
-router.post("/:uid", addTask);
+router.post("/:uid", protect, addTask);
 
 // @route   PUT /api/usertasks/:uid/:tid
 // @desc    Update a task
-router.put("/:uid/:tid", updateTask);
+router.put("/:uid/:tid", protect, updateTask);
 
 // @route   DELETE /api/usertasks/:uid/:tid
 // @desc    Delete a task
-router.delete("/:uid/:tid", deleteTask);
+router.delete("/:uid/:tid", protect, deleteTask);
 
 module.exports = router;
