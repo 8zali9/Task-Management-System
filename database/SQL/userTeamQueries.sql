cb0453aec00aa2a0ca2c04e762535fbe48c074db
Zulfiqar Ali <8zali9@gmail.com>
2023-11-14 18:29:33 +0500

    Queries for workGroups. Some remaining.

--- database/SQL/userTeamQueries.sql
+++ database/SQL/userTeamQueries.sql
@@ -9,4 +9,22 @@ select * from workGroupMembership
 where userID = 1 and workGroupID = 1;
 
 -- create a workGroup
-insert into workGroup ()
+insert into workGroup (workGroupName, workGroupMotive, workGroupLeadID)
+values ("wg", "aesi ji", 2);
+
+-- Update workGroup
+UPDATE workGroup
+SET workGroupName = 'newName', workGroupMotive = 'newMotive'
+WHERE workGroupID = 2 AND workGroupLeadID = (
+    SELECT workGroupLeadID FROM (SELECT * FROM workGroup) AS temp
+    WHERE workGroupID = 2
+);
+
+-- delete a workGroup
+delete from workGroup
+WHERE workGroupID = 3 AND workGroupLeadID = (
+    SELECT workGroupLeadID FROM (SELECT * FROM workGroup) AS temp
+    WHERE workGroupID = 3
+);
+
+-- adding users to the group
