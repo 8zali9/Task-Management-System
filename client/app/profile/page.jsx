f3b6843ba84b9e48f0ed5746aee3d03bfed87791
Zulfiqar Ali <8zali9@gmail.com>
2023-12-23 21:50:14 +0500

    Route protection hurdle to be countered.

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -1,7 +1,38 @@
+"use client"
+
 import React from 'react'
 
-export default function page() {
+const getUserDetails = async (userID) => {
+  const response = await fetch(`http://localhost:2424/api/user/${userID}`)
+  
+  const userDetails = await response.json()
+
+  return userDetails
+}
+
+export default async function page({userID}) {
+  const userDetails = await getUserDetails(userID)
+
+  if ((!Array.isArray(userDetails)) || (userDetails.length === 0)) {
+    return (
+      <div className='User Doesnt exist'>Problem fetching details.</div>
+    )
+  }
+
   return (
-    <div className='profile-pg'>profile</div>
+    <div className='profile-pg'>
+      <div className='user-details'>
+        {userDetails.map((i) => (
+          <div key={i.userID}>
+            <p>Name: {i.userName}</p>
+            <p>Email: {i.userEmail}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
+// signOutUser,
+// getUser,
+// updateUser,
+// deleteUser,
