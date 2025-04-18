98c77aa1fd997d30021f5ecebcb1006ecf44d88c
zulfiqar-emerssive <zulfiqar.ali@emerssive.com>
2025-04-19 03:52:36 +0500

    refactor initial

---
+++ server/controllers/workGroupControllers.js
@@ -0,0 +1,71 @@
+const mysql2 = require("mysql2");
+require("dotenv").config();
+const db = require("../config/connect_db");
+const {
+  qGetAllworkGroupsOfUser,
+  qGetAworkGroupOfUser,
+  qCreateworkGroup,
+  qUpdateworkGroup,
+  qDeleteworkGroup,
+} = require("../sql/teamQueries");
+
+// @route   POST /api/usergroups/:uid
+// @desc    Create a group of a user
+const createWorkGroup = async (req, res) => {
+  const workGroupLeadID = req.params.uid;
+  const { workGroupName, workGroupMotive } = req.body;
+
+  db.query(
+    qCreateworkGroup,
+    [workGroupName, workGroupMotive, workGroupLeadID],
+    (err, result) => {
+      if (err) {
+        res.status(400).json({ error: "Cannot create work group." });
+      } else {
+        const wid = result.workGroupID;
+        res.status(201).json({
+          message: "Workgroup created",
+          wid,
+        });
+      }
+    }
+  );
+};
+
+// @route   PUT /api/usergroups/:uid/:wid
+// @desc    Update a group of a user
+const updateWorkGroup = async (req, res) => {
+  const uid = req.params.uid;
+  const wid = req.params.wid;
+  if (!req.body) {
+    res.status(400).json({ error: "req body not present" });
+  }
+  const { workGroupName, workGroupMotive } = req.body;
+
+  db.query(
+    qUpdateworkGroup,
+    [workGroupName, workGroupMotive, wid, uid],
+    (err, result) => {
+      if (err) {
+        res.status(400).json({ message: "Cannot update the work group" });
+      } else {
+        res.status(200).json({ wid });
+      }
+    }
+  );
+};
+
+// @route   DELETE /api/usergroups/:uid
+// @desc    Delete a group of a user
+const deleteWorkGroup = async (req, res) => {
+  const uid = req.params.uid;
+  const wid = req.params.wid;
+
+  db.query(qDeleteworkGroup, wid, uid, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: "Cannot delete WG" });
+    } else {
+      res.status(200).json({ message: "WG deleted" });
+    }
+  });
+};
